8a019153f2c2f8028848e2db6dfa1d797a6742ff
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2022-02-23 06:10:52 -0300

    Add styles in Login

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const LoginScreen = () => {
 	return (
-		<div>
-			<h1>LoginScreen</h1>
-		</div>
+		<>
+			<h3 className="auth__title">Login</h3>
+
+			<form>
+				<input
+					type="text"
+					placeholder="Email"
+					name="email"
+					className="auth__input"
+					autoComplete="off"
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					name="password"
+					className="auth__input"
+					autoComplete="off"
+				/>
+
+				<button type="submit" className="btn btn-primary btn-block mb-5">
+					Login
+				</button>
+
+				<Link to="/auth/register">Create new account</Link>
+			</form>
+		</>
 	);
 };
