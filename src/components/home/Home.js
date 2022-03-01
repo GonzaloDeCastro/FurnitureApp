e3c5f0ab7908e81919ba645ebd0a13d77e36a36c
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2022-03-01 20:33:32 -0300

    CSS styles added in Home Page and Products

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 export const Home = () => {
 	return (
-		<>
-			<h1>Home</h1>
-		</>
+		<div className="homeContainer">
+			<h2>Welcome</h2>
+			<h4>This is a system of add delete and modifications of suppliers and products.</h4>
+			<h4>
+				Once you are registered you can register products, modify them and also delete
+				them.
+			</h4>
+			<h4>You can also add suppliers, modify or delete them.</h4>
+			<h4>
+				The system is programmed with front-end code in React, El mismo se conecta a un
+				backend programado con Express en Nodejs
+			</h4>
+			<h4>And the database to which the backend connects is MongoDB.</h4>
+		</div>
 	);
 };
