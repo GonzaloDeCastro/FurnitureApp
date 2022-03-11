200181ef7b8fb8a93b55127e81606ebe2eb0c281
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2022-03-11 15:36:05 -0300

    CSS responsive is added

--- src/components/providers/ProviderList.js
+++ src/components/providers/ProviderList.js
@@ -4,29 +4,27 @@ import { ProviderItem } from './ProviderItem';
 export const ProvidersList = (props) => {
 	const { providerList, onDelete, onEdit } = props;
 	return (
-		<>
-			<div className="table-responsive">
-				<table>
-					<thead>
-						<tr>
-							<th>Company</th>
-							<th>First name</th>
-							<th>Last name</th>
-							<th>Email</th>
-							<th>Phone</th>
-							<th>Options</th>
-						</tr>
-					</thead>
-					{providerList.map((provider) => (
-						<ProviderItem
-							key={provider._id}
-							providerToShow={provider}
-							onDelete={onDelete}
-							onEdit={onEdit}
-						/>
-					))}
-				</table>
-			</div>
-		</>
+		<div className="tableResponsive">
+			<table>
+				<thead>
+					<tr>
+						<th>Company</th>
+						<th>First name</th>
+						<th>Last name</th>
+						<th>Email</th>
+						<th>Phone</th>
+						<th>Options</th>
+					</tr>
+				</thead>
+				{providerList.map((provider) => (
+					<ProviderItem
+						key={provider._id}
+						providerToShow={provider}
+						onDelete={onDelete}
+						onEdit={onEdit}
+					/>
+				))}
+			</table>
+		</div>
 	);
 };
