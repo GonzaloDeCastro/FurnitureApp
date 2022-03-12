b61780c32d329d6400b0d3304b85f60e8faedf11
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2022-03-12 20:02:45 -0300

    Text home is changed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Home = () => {
 	return (
-		<div /*  className="homeContainer" */>
+		<div className="homeContainer">
 			<h2>Welcome</h2>
 			<p>This is a system of add delete and modifications of suppliers and products.</p>
 			<p>
@@ -11,8 +11,8 @@ export const Home = () => {
 			</p>
 			<p>You can also add suppliers, modify or delete them.</p>
 			<p>
-				The system is programmed with front-end code in React, El mismo se conecta a un
-				backend programado con Express en Nodejs
+				The system is programmed with front-end code in React, it connects to a backend
+				programmed with Express in Nodejs
 			</p>
 			{/* <p>And the database to which the backend connects is MongoDB.</p> */}
 		</div>
