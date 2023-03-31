118db521d34410eeea5498ba1122519ce46f2564
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2023-03-31 12:32:04 -0300

    Responsive home section

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -2,9 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footer2" style={{ height: "5%" }}>
-      &copy; Copyright 2023 - Gonzalo De Castro
-    </div>
+    <div className="footer2">&copy; Copyright 2023 - Gonzalo De Castro</div>
   );
 };
 
