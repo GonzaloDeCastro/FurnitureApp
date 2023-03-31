4b55360c5cda5b7cc4ec0eb19dfd77912be90ab3
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2023-03-31 13:07:36 -0300

    Deploy github

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+import React from "react";
+import { Route } from "react-router-dom";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
-export const PrivateRoute = ({ isAuthenticated, component: Component, ...rest }) => {
-	return (
-		<Route
-			{...rest}
-			component={(props) =>
-				isAuthenticated ? <Component {...props} /> : <Redirect to="/auth/login" />
-			}
-		/>
-	);
+export const PrivateRoute = ({
+  isAuthenticated,
+  component: Component,
+  ...rest
+}) => {
+  return (
+    <Route
+      {...rest}
+      component={(props) =>
+        isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/auth/login" />
+        )
+      }
+    />
+  );
 };
