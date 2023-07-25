26676c1b16abd4081001fe76cb3512369b64b222
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2023-07-25 19:44:51 -0300

    search engine is added in providers and products

--- src/components/products/ProductsList.js
+++ src/components/products/ProductsList.js
@@ -1,50 +1,72 @@
+import { useState } from "react";
 import { ProductItem } from "./ProductItem";
 
 export const ProductsList = ({ productList, onDelete, onEdit }) => {
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <div className="headerTable">
-        <div
-          style={{
-            display: "flex",
-            width: "90%",
-          }}
-        >
-          <div className="titleFieldProduct">Product</div>
-          <div className="titleFieldProduct">Description</div>
-          <div className="titleFieldProduct">Price</div>
-          <div className="titleFieldProduct">Brand</div>
-        </div>
+  const [searchText, setSearchText] = useState("");
 
-        <div className="optionItem">Options</div>
+  const handleSearch = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const filteredProducts = productList.filter((product) => {
+    const productData =
+      `${product.name} ${product.description} ${product.price} ${product.brand}`.toLowerCase();
+    return productData.includes(searchText.toLowerCase());
+  });
+
+  return (
+    <div>
+      <input
+        style={{ marginTop: "10px" }}
+        type="text"
+        onChange={handleSearch}
+        value={searchText}
+        placeholder="Search product"
+      />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div className="headerTable">
+          <div
+            style={{
+              display: "flex",
+              width: "90%",
+            }}
+          >
+            <div className="titleFieldProduct">Product</div>
+            <div className="titleFieldProduct">Description</div>
+            <div className="titleFieldProduct">Price</div>
+            <div className="titleFieldProduct">Brand</div>
+          </div>
+
+          <div className="optionItem">Options</div>
+        </div>
+        {filteredProducts.length === 0 ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "0.5rem",
+            }}
+          >
+            {"There are no products matching the search criteria"}
+          </div>
+        ) : (
+          <div className="bodyTable">
+            {filteredProducts.map((product) => (
+              <ProductItem
+                key={product._id}
+                productToShow={product}
+                onDelete={onDelete}
+                onEdit={onEdit}
+              />
+            ))}
+          </div>
+        )}
       </div>
-      {productList.length === 0 ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "0.5rem",
-          }}
-        >
-          {"There are not products"}
-        </div>
-      ) : (
-        <div className="bodyTable">
-          {productList.map((product) => (
-            <ProductItem
-              key={product._id}
-              productToShow={product}
-              onDelete={onDelete}
-              onEdit={onEdit}
-            />
-          ))}
-        </div>
-      )}
     </div>
   );
 };
--- src/components/providers/ProviderList.js
+++ src/components/providers/ProviderList.js
@@ -1,51 +1,72 @@
+import { useState } from "react";
 import { ProviderItem } from "./ProviderItem";
 
 export const ProvidersList = ({ providerList, onDelete, onEdit }) => {
+  const [searchText, setSearchText] = useState("");
+  console.log("gonza ", providerList);
+  const handleSearch = (event) => {
+    setSearchText(event.target.value);
+  };
+  const filteredProviders = providerList.filter((provider) => {
+    const productData =
+      `${provider.company} ${provider.name} ${provider.email} ${provider.phone}`.toLowerCase();
+    return productData.includes(searchText.toLowerCase());
+  });
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <div className="headerTable">
-        <div
-          style={{
-            display: "flex",
-            width: "90%",
-          }}
-        >
-          <div className="titleFieldProvider">Company</div>
-          <div className="titleFieldProvider">Name</div>
-          <div className="titleFieldProvider">Phone</div>
-          <div className="titleFieldProvider">Email</div>
+    <div>
+      <input
+        style={{ marginTop: "10px" }}
+        type="text"
+        onChange={handleSearch}
+        value={searchText}
+        placeholder="Search provider"
+      />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div className="headerTable">
+          <div
+            style={{
+              display: "flex",
+              width: "90%",
+            }}
+          >
+            <div className="titleFieldProvider">Company</div>
+            <div className="titleFieldProvider">Name</div>
+            <div className="titleFieldProvider">Phone</div>
+            <div className="titleFieldProvider">Email</div>
+          </div>
+
+          <div className="optionItem">Options</div>
         </div>
 
-        <div className="optionItem">Options</div>
+        {filteredProviders.length === 0 ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "0.5rem",
+            }}
+          >
+            {"There are not providers"}
+          </div>
+        ) : (
+          <div className="bodyTable">
+            {filteredProviders.map((provider) => (
+              <ProviderItem
+                key={provider._id}
+                providerToShow={provider}
+                onDelete={onDelete}
+                onEdit={onEdit}
+              />
+            ))}
+          </div>
+        )}
       </div>
-
-      {providerList.length === 0 ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "0.5rem",
-          }}
-        >
-          {"There are not providers"}
-        </div>
-      ) : (
-        <div className="bodyTable">
-          {providerList.map((provider) => (
-            <ProviderItem
-              key={provider._id}
-              providerToShow={provider}
-              onDelete={onDelete}
-              onEdit={onEdit}
-            />
-          ))}
-        </div>
-      )}
     </div>
   );
 };
